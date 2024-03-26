6f8b7ba866ccd59f0e8034948f5644c2ebf5a120
Peter Arnold <peter.arnold254@gmail.com>
2024-03-26 18:42:43 +0300

    updated config

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 
 const app_port = process.env.APP_PORT;
 const app_name = process.env.APP_NAME;
-const origins = process.env.ORIGINS;
+const origins = JSON.parse(process.env.ORIGINS);
 
 module.exports = {
   app_port,
   app_name,
+  origins,
 };
